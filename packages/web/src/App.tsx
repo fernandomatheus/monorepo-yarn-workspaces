2b705b5443dda25d3504373af5c78dc71096c23e
Fernando Matheus <osf.fmatheus@creditas.com>
2023-10-13 16:00:57 -0300

    feat: reusing shared modules

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
+
+// import api from '@monorepo/axios-config'
 
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
-};
+  return <h1>Hello World</h1>
+}
 
-export default App;
+export default App
